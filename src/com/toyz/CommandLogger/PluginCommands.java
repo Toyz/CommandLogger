8620582ac0767c8508e0288227cc0a4a0a2e1fbe
Travis McMillan <toyzz@live.com>
2014-06-09 20:17:57 -0700

    Added  Purge command
    coml.purge is the needed permission to use this command unless op

--- src/com/toyz/CommandLogger/PluginCommands.java
+++ src/com/toyz/CommandLogger/PluginCommands.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 
 public class PluginCommands implements CommandExecutor{
 	CommandLogger main = null;
+	Boolean is_Dumping = false;
 	public PluginCommands(CommandLogger instance){
 		main = instance;
 	}
@@ -21,15 +22,31 @@ public class PluginCommands implements CommandExecutor{
 	@Override
     public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings) {
 		String player = "Console";
+		Boolean is_Console = commandSender instanceof Player;
+		ConsoleCommandSender console = commandSender.getServer().getConsoleSender();
         if (command.getName().equalsIgnoreCase("coml")) {
-        	if (commandSender instanceof Player) {
-        		player = ((Player) commandSender).getDisplayName();
-        		commandSender.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
-        		commandSender.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
-        	}else{
-        		ConsoleCommandSender console = commandSender.getServer().getConsoleSender();
-        		console.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
-        		console.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
+        	switch(strings[0].toLowerCase()){
+        		case "purge":
+        			if(is_Console){
+        				if (commandSender.hasPermission("coml.purge")) {
+        					PurgeServerLogs();
+        					commandSender.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
+        				}else{
+        					PurgeServerLogs();
+        					console.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
+        				}
+        			}
+        		break;
+        		default:
+        			if (is_Console) {
+                		player = ((Player) commandSender).getDisplayName();
+                		commandSender.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
+                		commandSender.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
+                	}else{
+                		console.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
+                		console.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
+                	}
+                	break;
         	}
         }
         if (commandSender instanceof Player) {
@@ -38,14 +55,28 @@ public class PluginCommands implements CommandExecutor{
         File PlayerText = new File(CommandLogger.FolderPath + File.separator + player + ".txt");
  	   
  	   String message = s;
- 	   System.out.println(message);
- 	   Date now = new Date();
- 		   try {
- 			FileUtils.writeStringToFile(PlayerText, "[" + now +"]" + message + "\r\n", true);
- 		} catch (IOException e) {
- 			// TODO Auto-generated catch block
- 			e.printStackTrace();
- 		}
+ 	   if(!is_Dumping){
+	 	   Date now = new Date();
+	 		   try {
+	 			FileUtils.writeStringToFile(PlayerText, "[" + now +"]" + message + "\r\n", true);
+	 		} catch (IOException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		}
+ 	    }
         return true;
+	}
+	
+	private void PurgeServerLogs(){
+		is_Dumping = true;
+		File file = new File(CommandLogger.FolderPath);  
+		 String[] LogFiles;      
+         if(file.isDirectory()){  
+        	 LogFiles = file.list();  
+             for (int i=0; i<LogFiles.length; i++) {  
+                 File myFile = new File(file, LogFiles[i]);   
+                 myFile.delete();  
+             }  
+         }
 	}
 }
