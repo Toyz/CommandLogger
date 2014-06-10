a47b879fb7ffd75df16512bd8db985b7da8c747e
Travis McMillan <toyzz@live.com>
2014-06-09 21:21:41 -0700

    Added Backing up to zipfile and purging on back up

--- src/com/toyz/CommandLogger/CommandLogger.java
+++ src/com/toyz/CommandLogger/CommandLogger.java
@@ -6,12 +6,17 @@ import java.io.*;
 
 public class CommandLogger extends JavaPlugin {
 	public static String FolderPath = null;
+	public static String BackUpPath = null;
 	public void onEnable() {
 		 getDataFolder().mkdir();
 		 if(!new File(getDataFolder() + File.separator  + "Commands").exists()){
 			 new File(getDataFolder() + File.separator  + "Commands").mkdir(); 
 		 }
+		 if(!new File(getDataFolder() + File.separator  + "BackUps").exists()){
+			 new File(getDataFolder() + File.separator  + "BackUps").mkdir(); 
+		 }
 		 FolderPath = getDataFolder() + File.separator  + "Commands";
+		 BackUpPath = getDataFolder() + File.separator  + "BackUps";
 		 PluginManager pm = getServer().getPluginManager();
 		 
 		 pm.registerEvents(new CommandListener(this), this);
--- src/com/toyz/CommandLogger/PluginCommands.java
+++ src/com/toyz/CommandLogger/PluginCommands.java
@@ -1,15 +1,14 @@
 package com.toyz.CommandLogger;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.zip.*;
 
-import net.minecraft.util.org.apache.commons.io.FileUtils;
+import net.minecraft.util.org.apache.commons.io.*;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class PluginCommands implements CommandExecutor{
@@ -25,28 +24,90 @@ public class PluginCommands implements CommandExecutor{
 		Boolean is_Console = commandSender instanceof Player;
 		ConsoleCommandSender console = commandSender.getServer().getConsoleSender();
         if (command.getName().equalsIgnoreCase("coml")) {
-        	switch(strings[0].toLowerCase()){
-        		case "purge":
-        			if(is_Console){
-        				if (commandSender.hasPermission("coml.purge")) {
-        					PurgeServerLogs();
-        					commandSender.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
-        				}else{
+        	if(strings.length <= 0){
+        		if (is_Console) {
+            		player = ((Player) commandSender).getDisplayName();
+            		commandSender.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
+            		commandSender.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
+            	}else{
+            		console.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
+            		console.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
+            	}
+        	}else{
+	        	switch(strings[0].toLowerCase()){
+	        		case "purge":
+	        			if(is_Console){
+	        				if (commandSender.hasPermission("coml.purge")) {
+	        					PurgeServerLogs();
+	        					commandSender.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
+	        				}else{
+	        					PurgeServerLogs();
+	        					console.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
+	        				}
+	        			}
+	        		break;
+	        		case "backup":
+	        			Date date = new Date() ;
+        				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
+	        			if(is_Console){
+	        				File backup = new File(CommandLogger.BackUpPath, dateFormat.format(date) + ".zip");
+	        				FileOutputStream fos = null;
+							try {
+								fos = new FileOutputStream(backup);
+							} catch (FileNotFoundException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	        				BufferedOutputStream bos = new BufferedOutputStream(fos);
+	        				ZipOutputStream zos = new ZipOutputStream(bos);
+	        				if(commandSender.hasPermission("coml.backup")){
+	        					try {
+									addFolderToZip(new File(CommandLogger.FolderPath), zos, "/");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+	        					try {
+									zos.close();
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+	        					commandSender.sendMessage(ChatColor.RED + "Back up for " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs has finished");
+	        					if (commandSender.hasPermission("coml.purge"))
+	        						PurgeServerLogs();
+	        					commandSender.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
+	        				}
+	        			}else{
+	        				File backup = new File(CommandLogger.BackUpPath, dateFormat.format(date) + ".zip");
+	        				FileOutputStream fos = null;
+							try {
+								fos = new FileOutputStream(backup);
+							} catch (FileNotFoundException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	        				BufferedOutputStream bos = new BufferedOutputStream(fos);
+	        				ZipOutputStream zos = new ZipOutputStream(bos);
+	        				try {
+								addFolderToZip(new File(CommandLogger.FolderPath), zos, "/");
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	        				try {
+								zos.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	        		
+        					console.sendMessage(ChatColor.RED + "Back up for " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs has finished");
         					PurgeServerLogs();
         					console.sendMessage(ChatColor.RED + "Dumped " + ChatColor.AQUA + "CommandLogger" + ChatColor.RED + " logs");
-        				}
-        			}
-        		break;
-        		default:
-        			if (is_Console) {
-                		player = ((Player) commandSender).getDisplayName();
-                		commandSender.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
-                		commandSender.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
-                	}else{
-                		console.sendMessage(ChatColor.AQUA + " -> CommandLogger <- ");
-                		console.sendMessage(ChatColor.RED + " -> Created By, Toyz <- ");
-                	}
-                	break;
+	        			}
+	        			break;
+	        	}
         	}
         }
         if (commandSender instanceof Player) {
@@ -79,5 +140,20 @@ public class PluginCommands implements CommandExecutor{
                  myFile.delete();  
              }  
          }
+	}
+	
+	private void addFolderToZip(File folder, ZipOutputStream zip, String baseName) throws IOException {
+	    File[] files = folder.listFiles();
+	    for (File file : files) {
+	        if (file.isDirectory()) {
+	            addFolderToZip(file, zip, baseName);
+	        } else {
+	            String name = file.getAbsolutePath().substring(baseName.length());
+	            ZipEntry zipEntry = new ZipEntry(name);
+	            zip.putNextEntry(zipEntry);
+	            IOUtils.copy(new FileInputStream(file), zip);
+	            zip.closeEntry();
+	        }
+	    }
 	}
 }
